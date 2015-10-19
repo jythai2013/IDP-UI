36c8dfa8c900dff4d17c5e193a0dd1b8ca8007b2
Matthew <asdf154@gmail.com>
2015-10-19 17:10:05 +0800

    Added some events to fullCalendar

--- client/lib/js/calendar.js
+++ client/lib/js/calendar.js
@@ -9,7 +9,24 @@
 Template.haha.helpers({
 	options: function() {
 			return {
-					defaultView: 'agendaWeek'
+					defaultView: 'agendaWeek',
+					events: [
+						{
+								title  : 'event1',
+								start  : '2015-10-19'
+						},
+						{
+								title  : 'event2',
+								start  : '2015-10-18T10:00:00',
+								end		 : '2015-10-20T12:00:00',
+								allDay : false 
+						},
+						{
+								title  : 'event3',
+								start  : '2015-10-25T12:30:00',
+								allDay : false // will make the time show
+						}
+					]
 			};
 	}
 });
